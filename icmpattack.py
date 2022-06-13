3611088c258c8b2c4ab086fa6ac3b89006391f51
Dhruva Santosh <dhruvasantosh16@outlook.com>
2022-06-13 08:30:20 -0700

    Basic Structure

---
+++ icmpattack.py
@@ -0,0 +1,28 @@
+def welcome():
+    print("Welcome to ICMP Attack Suite!", "1. Set ICMP Parameters", "2. Set ICMP Payload", "3. Start Attack", "4. Exit", sep='\n', end='\n')
+    sel1 = input("Your Selection: ")
+    if(sel1 == "1"):
+        icmpparams()
+    elif(sel1 == "2"):
+        payloads()
+    elif(sel1 == "3"):
+        exploit()
+    elif(sel1 == "4"):
+         exit()
+    else:
+        print("Invalid Option ")
+
+def icmpparams():
+    print("Add Parameters here")
+
+def payloads():
+    print("Add Payloads here")
+
+def exploit():
+    print("Add Exploit Block here")
+
+def main():
+    welcome()
+
+if __name__ == "__main__":
+    main()
