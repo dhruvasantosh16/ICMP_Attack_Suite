bd083266ab5ba95c947516edbc7fe4433d789613
Dhruva Santosh <dhruvasantosh16@outlook.com>
2022-06-14 19:21:22 +0100

    flow based instead of list based

--- icmpattack.py
+++ icmpattack.py
@@ -1,8 +1,10 @@
+from encodings import utf_8
 from impacket import ImpactDecoder
 from impacket import ImpactPacket
 import socket
 ipheader = ImpactPacket.IP()
 icmpheader = ImpactPacket.ICMP()
+payload = "Standard Reverse Shell"
 
 def welcome():
     #Dependency Check
@@ -12,42 +14,41 @@ def welcome():
         print("Impacket library is missing (Run", "pip install impacket)")
         exit(1)
     #Welcome Prompt
-    print('\n', "Welcome to ICMP Attack Suite!", '\n', "1. Set ICMP Parameters", "2. Set ICMP Payload", "3. Start Attack", "4. Exit", '\n', sep='\n', end='\n')
-    sel1 = input("Your Selection: ")
-    if(sel1 == "1"):
-        icmpparams()
-    elif(sel1 == "2"):
-        payloads()
-    elif(sel1 == "3"):
-        exploit()
-    elif(sel1 == "4"):
-         exit()
-    else:
-        print("Invalid Option ")
-
-def icmpparams():
-    setIP()
+    print('\n', "Welcome to ICMP Attack Suite!", '\n', "ICMP Parameters", '\n', sep='\n')
     setICMP()
 
 def payloads():
-    print("Payload Block")
+    print("Payload Selection", '\n', "1. Reverse Shell", "2. OS Fingerprinting", "3. Ping", "4. Back to Main Menu", '\n', sep='\n')
+    pl = input("Selection: ")
+    if (pl == "1"):
+        print('\n', "Select OS", '\n', "1. POSIX System", "2. Windows System", '\n', sep='\n')
+        plsel = input("Selection: ")
+        if (plsel == "1"):
+            print("Python Reverse Shell Command for Linux")
+        elif (plsel == "2"):
+            print("Python Reverse Shell Command for Windows")
+        else:
+            print("Invalid Option")
+    else:
+        print("Invalid Option")
+    payload_b = bytes(payload, encoding="utf_8")
+    icmpheader.contains(ImpactPacket.Data(payload_b))
+    ipheader.contains(icmpheader)
+    print(ipheader)
 
 def exploit():
-    print("Add Exploit Block here")
+    
 
-def setIP():
+def setICMP():
     dst = input("Enter Victim IP address: ")
+    print('\n')
     host = socket.gethostname()
     src = socket.gethostbyname(host)
     ipheader.set_ip_dst(dst)
     ipheader.set_ip_src(src)
-    print('\n', "IP Header: ", ipheader, '\n')
-    return(ipheader)
-
-def setICMP():
     icmpheader.set_icmp_type(icmpheader.ICMP_ECHO)
+    payloads()
     
-
 def main():
     welcome()
 
